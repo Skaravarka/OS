e6450a590c2da35d9cb3704debaa769743637be8
Skaravarka <juozapasnorvaisas@gmail.com>
2020-04-01 13:30:40 +0300

    uwu

--- 2ND/Main.java
+++ 2ND/Main.java
@@ -10,8 +10,13 @@ public class Main {
 
         VirtualMachine virtualMachine = new VirtualMachine(0, 0, 0, 0, 0, 0);
         
-        virtualMachine.loadToMemory("2ND/PROG1.txt");
-        
+        //virtualMachine.loadToMemory("2ND/PROG1.txt");
+
+        Memory mem = new Memory();
+        System.out.println(mem.loadToMemory("2ND/PROGURAMUUWU.txt"));
+
+
+
     }
 
 }
--- 2ND/Memory.java
+++ 2ND/Memory.java
@@ -1,9 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Memory{
     private ArrayList<Word> virtualMemory = new ArrayList<Word>();
 
     public void PrintAll(){
-        System.out.println(this.virtualMemory);
+       for(int i = 0; i < this.virtualMemory.size();i++){
+           System.out.println(Word.wordToString(this.virtualMemory.get(i)));
+       }
+    }
+
+    public int loadToMemory(String fileName) {
+        File file = new File(fileName);
+        Scanner scanner;
+        int segmentFlag = 0; //DATA CODE HALT
+        try {
+            scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                String data = scanner.nextLine();
+                data.toUpperCase();
+                if(data.equals("CODE")){
+                    segmentFlag = 2;
+                    continue;
+                }
+                if(data.equals("HALT")){
+                    scanner.close();
+                    return -1;
+                }
+                if(segmentFlag == 1){
+                    System.out.println(data);
+                    String[] parts = data.split(" ");
+                    //virtualMemory.set(Integer.parseInt(parts[1]), parts[2]);
+                    //System.out.println("final" + Word.wordToString(Word.stringToWord("ABCD")));
+                }
+            }
+            scanner.close();
+            return -1;
+
+        } catch (FileNotFoundException e) {
+            
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
